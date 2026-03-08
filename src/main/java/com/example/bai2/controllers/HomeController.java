636399fe71f42576ef4836d36df4194401a19852
Nguyen Minh Dung <bulime911@gmail.com>
2026-03-08 15:44:32 +0700

    buoi5

---
+++ src/main/java/com/example/bai2/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.bai2.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String index(Model model) {
+        Map<String, String> home = new HashMap<>();
+        home.put("title", "Chiu thua cach noi yeu em");
+        home.put("message", "Xin caho");
+        model.addAttribute("home", home);
+        return "index";
+    }
+}
